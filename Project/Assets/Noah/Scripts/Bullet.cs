9c7cb9b1840dae198001239676828647ee736ca4
epicacid <noah@Noahs-MacBook-Pro.local>
2016-11-22 16:22:46 +0100

    Shoot

---
+++ Project/Assets/Noah/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	void Start(){
+		Destroy (gameObject, 3f);
+	}
+
+	public void BulletRight(){
+		transform.Translate (transform.right * 0.2f);
+	}
+
+
+}
--- Project/Assets/Noah/Scripts/PlayerShoot.cs
+++ Project/Assets/Noah/Scripts/PlayerShoot.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class PlayerShoot : MonoBehaviour {
 
-	// Use this for initialization
+	public GameObject bullet;
+	[SerializeField]
+	Transform bulletSpwn;
+	[SerializeField]
+	private float _bulletSpeed;
+
+
+
 	void Start () {
-	
+		//bullet = gameObject.GetComponent<Rigidbody2D> ();
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate(){
+		if (Input.GetKeyDown (KeyCode.L)) {
+			GameObject bullInstance = Instantiate (bullet, bulletSpwn.position, bulletSpwn.rotation) as GameObject;
+		}
+
+
+
+
 	}
 }
+
